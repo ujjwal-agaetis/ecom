425fed7e300e1aeeb48c13171a0002bfade738ab
Ujjwal Dubey <ujjwal.d@agaetis.tech>
2023-08-08 14:56:43 +0530

    bugs

--- resources/views/category/product_list.blade.php
+++ resources/views/category/product_list.blade.php
@@ -1,21 +1,37 @@
-<!-- resources/views/products/product_list.blade.php -->
+<!-- resources/views/category/product_list.blade.php -->
 @extends('layouts.app')
 @section('content')
+<!-- {{dd($products)}} -->
+ $category_id=$products->id;
+           <!-- {{$category_id}} -->
+                    @if($category_id==1)
+                    $category_img='cloth_default.jpg';
+
+                     @elseif($category_id==2)
+                      $category_img='realme_default.jpg';
+
+                      @else
+                      $category_img='grocerys_default.jpg';
+
+                     @endif
+
 <div class="container">
     <h1>Product Details category wise</h1>
     <div class="row gap-3">
         @foreach($products->product_list as $product)
+        
         <div class="col-3 d-flex align-items-strech">
             <!-- bootstrap cards code here -->
             <div class="card" style="width: 18rem;">
                 <form class="add_cart_form_{{$product->id}}">
                     @csrf
-                    <img class="card-img-top" src="https://odoo-community.org/web/image/product.template/3936/image_1024?unique=88a9e3e" alt="Card image cap">
+                    <img src="{{ asset('img/category/realme_default.jpg') }}" width="260px" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">{{ $product->name }}</h5>
                         <p class="card-text">{{ $product->description }}</p>
                         <p class="card-text">Available Quantity <span class="badge text-bg-primary">{{$product->quantity}}</span></p>
                         <button class="btn btn-primary text-end add_to_cart" data-id="{{$product->id}}">Add to cart</button>
+                        
                         @if (!empty($product->quantity))
                             Qty <input type="number" name="quantity" style="width:45px;" value="1">
                         @else
@@ -31,7 +47,7 @@
         @endforeach
     </div>
 </div>
-<a href="{{ route('products.index') }}" name="show">View Product Details</a>
+<!-- <a href="{{ route('products.index') }}" name="show">View Product Details</a> -->
 <script type="module">
     $(document).ready(function() {
         // add cart action for category
